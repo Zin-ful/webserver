32a305c481cf18950010bd87766fc5f721e34830
Zinful <zinful@tutamail.com>
2025-04-15 13:48:13 -0400

    Delete webserver directory

--- webserver/handle_files.h
+++
@@ -1,8 +0,0 @@
-#ifndef HANDLE_FILES_H
-#define HANDLE_FILES_H
-
-void index_folder(const char *query, const char *dir, char *output, size_t max_output);
-
-void capture_suggest(const char *suggestion);
-
-#endif
