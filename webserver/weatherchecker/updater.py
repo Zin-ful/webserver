09781c3fcbb9b7e8a2842cf43e38ac6037bafeef
Zinful <zinful@tutamail.com>
2025-05-19 16:29:23 -0400

    Update updater.py

--- webserver/weatherchecker/updater.py
+++ webserver/weatherchecker/updater.py
@@ -3,13 +3,21 @@
 import requests
 link1 = "https://raw.githubusercontent.com/Zin-ful/webserver/refs/heads/main/webserver/weatherchecker/weathertool.py"
 link2 = "https://raw.githubusercontent.com/Zin-ful/webserver/refs/heads/main/webserver/weatherchecker/weathertool-backgroundproc.py"
-print("What would you like to update?\n\n1. Weather Tool\n2.Weather Tool Web")
+print("What would you like to update?\n\n1. Weather Tool\n2. Weather Tool Web")
 def updateme(link, name):
-	newfile = requests.get(link)
+	response = requests.get(link)
 	errors = newfile.raise_for_status()
-	with open(name, "w") as file:
-		file.write(str(newfile))
-
+	if response.status_code == 200:
+		with open(name, "w") as file:
+			file.write(response.content)
+	elif response.status_code == 400:
+		print(f"\n****Err: {response.status_code}, code 400 points to the link being incorrect or it could be a server-side issue")
+	elif response.status_code == 404:
+		print(f"\n****Err: {response.status_code}, code 404 points to the link/file being unavalible or the site is down/no longer maintained")
+	elif response.status_code == 408:
+		print(f"\n****Err: {response.status_code}, code 408 points to your request not reaching the site before it timed out")
+	else:
+		print(f"\n****Err: {response.status_code}")
 while True:
 	inp = input(">>> ")
 	if "1" in inp or inp == "Weather Tool" or inp == "weather tool" or inp == "WeatherTool" or inp == "weathertool":
