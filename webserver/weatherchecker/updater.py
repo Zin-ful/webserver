d584facaee8e57d9a7b1fb337aa6109416f24afa
Zinful <zinful@tutamail.com>
2025-05-19 16:19:28 -0400

    Update updater.py

--- webserver/weatherchecker/updater.py
+++ webserver/weatherchecker/updater.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 
-import subprocess
+import requests
 link1 = "https://raw.githubusercontent.com/Zin-ful/webserver/refs/heads/main/webserver/weatherchecker/weathertool.py"
 link2 = "https://raw.githubusercontent.com/Zin-ful/webserver/refs/heads/main/webserver/weatherchecker/weathertool-backgroundproc.py"
 print("What would you like to update?\n\n1. Weather Tool\n2.Weather Tool Web")
-def updateme(link):
-	subprocess.run(f"wget {link}", shell=True)
+def updateme(link, name):
+	newfile = requests.get(link)
+	errors = newfile.raise_for_status()
+	with open(name, "w") as file:
+		file.write(newfile)
+
 while True:
 	inp = input(">>> ")
 	if "1" in inp or inp == "Weather Tool" or inp == "weather tool" or inp == "WeatherTool" or inp == "weathertool":
-		updateme(link1)
+		updateme(link1, "weathertool.py")
 	elif "2" in inp or "web" in inp or "Web" in inp or "WEB" in inp or "wEB" in inp:
-		updateme(link2)
+		updateme(link2, "wt-background.py")
 	else:
 		print("Sorry but that isnt an option")
 		continue
-
